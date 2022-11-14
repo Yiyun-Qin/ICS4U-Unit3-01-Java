a4ebf656bd7870db62dea6ba6364075d4f5f60bb
Yiyun Qin <yiyun.qin@mths.ca>
2022-11-14 03:48:11 +0000

    recursion

---
+++ Recursion.java
@@ -0,0 +1,29 @@
+/**
+ * The function reverses a string.
+ *
+ * @author  Yiyun Qin
+ * @version 1.0
+ * @since 2022-11-13
+ *
+ */
+
+class Recursion {
+  static String reverse(String string) {
+    StringBuilder remove = new StringBuilder(string);
+    if (string.isEmpty()) {
+      return string;
+    } else {
+      return reverse(string.substring(1)) + string.charAt(0);
+    }
+  }
+
+  public static void main(String[] args) {
+    String string = "recusrsion";
+
+    System.out.println("");
+    System.out.println("The original string is " + string);
+    System.out.println("The reversed string is " + reverse(string));
+    System.out.println("\nDone.");
+  }
+}
+  
